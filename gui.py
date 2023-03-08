27e67a289282450c693fd8147c54f595a4573a23
aliriza <aliriza.yilmaz@outlook.com.au>
2023-03-08 14:58:02 +0000

    Text file creator is implemented.

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("toDos.txt"):
+    with open("toDos.txt", "w") as file:
+        pass
 
 sg.theme("DarkBlack")
 
